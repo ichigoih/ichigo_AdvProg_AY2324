99f2b848f7ee5b8867a12200f88a5b338d30f4ed
ichigoih <nongami87@gmail.com>
2024-03-12 23:46:43 +0700

    update hello.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -50,7 +50,7 @@ string getAnswer(int number, int randomNumber) {
     string answer;
 
     if (number > randomNumber)
-        answer = "Your name is higher.";
+        answer = "Your number is higher.";
     if (number < randomNumber)
         answer = "Your number is lower.";
     else answer = "Congratulation! You win.";
