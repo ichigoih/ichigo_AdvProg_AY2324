ad10540afd0a91f5356c6bba37f2b8aa94c83573
ichigoih <nongami87@gmail.com>
2024-03-13 15:19:52 +0700

    update guessit.cpp

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -83,7 +83,7 @@ bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
 
-    if (isContinued == 't')
+    if (isContinued == 'Y')
         result = true;
 
     return result;
