1598e1284078d18deedd9b776dcd2df8455c967e
ichigoih <nongami87@gmail.com>
2024-04-10 15:14:41 +0700

    update painter.cpp

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -49,7 +49,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter
-    angle += degree;
+    this->angle += degree;
 }
 
 
@@ -62,7 +62,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter
-    angle -= degree;
+    this->angle -= degree;
 }
 
 /***  
